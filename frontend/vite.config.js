99cf0eec3aade6ef540137669489bcb9422f4350
hak0622 <seounghak062@naver.com>
2026-04-17 21:43:08 +0900

    fix: oauth redirect and remove login proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,29 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
-      '/user': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
-      '/oauth2': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
-      '/login': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
-      '/logout': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            },
+            '/user': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            },
+            '/oauth2': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            },
+            '/logout': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            },
+        },
     },
-  },
 })
